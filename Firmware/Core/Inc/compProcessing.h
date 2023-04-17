312949c66d7ff5ff8c75274fea31b3111b0fa384
E-Eidt <evaneidt@gmail.com>
2023-04-17 15:18:07 -0400

    Adding Comp Filter files

---
+++ Firmware/Core/Inc/compProcessing.h
@@ -0,0 +1,13 @@
+/*
+ * compProcessing.h
+ *
+ *  Created on: Apr 17, 2023
+ *      Author: evanm
+ */
+
+#ifndef INC_COMPPROCESSING_H_
+#define INC_COMPPROCESSING_H_
+
+
+
+#endif /* INC_COMPPROCESSING_H_ */
---
+++ Firmware/Core/Src/compProcessing.c
@@ -0,0 +1,8 @@
+/*
+ * compProcessing.c
+ *
+ *  Created on: Apr 17, 2023
+ *      Author: evanm
+ */
+
+
